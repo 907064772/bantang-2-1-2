0f0e36479b74822fa31cabaf81bba1150492e44b
MS <ms@love.local>
2015-12-28 09:37:17 +0800

    Initial Commit

---
+++ bantang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bantang
+//
+//  Created by MS on 15-12-28.
+//  Copyright (c) 2015年 ms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bantang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  bantang
+//
+//  Created by MS on 15-12-28.
+//  Copyright (c) 2015年 ms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
